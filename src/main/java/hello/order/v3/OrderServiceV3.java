9aa7e5ca946180d08ea99e0747291221ea01d224
nullpark <pnr9255@gmail.com>
2024-07-28 19:01:06 +0900

    [feat] OrderServiceV3 : MeterRegistry 를 이용한 커스텀 타이머 등록 - 총 요청 시간, 총 요청 수, 최대 요청 시간 조회 가능

---
+++ src/main/java/hello/order/v3/OrderServiceV3.java
@@ -0,0 +1,62 @@
+package hello.order.v3;
+
+import hello.order.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OrderServiceV3 implements OrderService {
+
+    private final MeterRegistry registry;
+    private AtomicInteger stock = new AtomicInteger(1000);
+
+    @Override
+    public void order() {
+        Timer timer = Timer.builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tag("method", "order")
+                .description("order")
+                .register(registry);
+
+        timer.record(() ->{
+            log.info("주문");
+            stock.decrementAndGet();
+            sleep(500);
+        });
+    }
+
+    @Override
+    public void cancel() {
+        Timer timer = Timer.builder("my.order")
+                .tag("class", this.getClass().getName())
+                .tag("method", "cancel")
+                .description("order")
+                .register(registry);
+
+        timer.record(() -> {
+            log.info("취소");
+            stock.incrementAndGet();
+            sleep(200);
+        });
+
+    }
+
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+
+    private static void sleep(int time) {
+        try {
+            Thread.sleep(time + new Random().nextInt(200));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
